6fea00b021d7bb1f629a7080cd056e525d506db5
jackelaide <j.l.fabian91@gmail.com>
2018-12-24 06:49:03 +1030

    Update IRrecvDumpV2.ino

--- examples/IRrecvDumpV2/IRrecvDumpV2.ino
+++ examples/IRrecvDumpV2/IRrecvDumpV2.ino
@@ -26,19 +26,8 @@
 #include <IRremoteESP8266.h>
 #include <IRutils.h>
 // The following are only needed for extended decoding of A/C Messages
-#include <ir_Coolix.h>
-#include <ir_Daikin.h>
-#include <ir_Fujitsu.h>
-#include <ir_Gree.h>
-#include <ir_Haier.h>
 #include <ir_Hitachi.h>
-#include <ir_Kelvinator.h>
-#include <ir_Midea.h>
-#include <ir_Mitsubishi.h>
-#include <ir_Panasonic.h>
-#include <ir_Samsung.h>
-#include <ir_Toshiba.h>
-#include <ir_Whirlpool.h>
+
 
 
 // ==================== start of TUNEABLE PARAMETERS ====================
@@ -116,91 +105,6 @@ decode_results results;  // Somewhere to store the results
 // Display the human readable state of an A/C message if we can.
 void dumpACInfo(decode_results *results) {
   String description = "";
-#if DECODE_DAIKIN
-  if (results->decode_type == DAIKIN) {
-    IRDaikinESP ac(0);
-    ac.setRaw(results->state);
-    description = ac.toString();
-  }
-#endif  // DECODE_DAIKIN
-#if DECODE_FUJITSU_AC
-  if (results->decode_type == FUJITSU_AC) {
-    IRFujitsuAC ac(0);
-    ac.setRaw(results->state, results->bits / 8);
-    description = ac.toString();
-  }
-#endif  // DECODE_FUJITSU_AC
-#if DECODE_KELVINATOR
-  if (results->decode_type == KELVINATOR) {
-    IRKelvinatorAC ac(0);
-    ac.setRaw(results->state);
-    description = ac.toString();
-  }
-#endif  // DECODE_KELVINATOR
-#if DECODE_MITSUBISHI_AC
-  if (results->decode_type == MITSUBISHI_AC) {
-    IRMitsubishiAC ac(0);
-    ac.setRaw(results->state);
-    description = ac.toString();
-  }
-#endif  // DECODE_MITSUBISHI_AC
-#if DECODE_TOSHIBA_AC
-  if (results->decode_type == TOSHIBA_AC) {
-    IRToshibaAC ac(0);
-    ac.setRaw(results->state);
-    description = ac.toString();
-  }
-#endif  // DECODE_TOSHIBA_AC
-#if DECODE_GREE
-  if (results->decode_type == GREE) {
-    IRGreeAC ac(0);
-    ac.setRaw(results->state);
-    description = ac.toString();
-  }
-#endif  // DECODE_GREE
-#if DECODE_MIDEA
-  if (results->decode_type == MIDEA) {
-    IRMideaAC ac(0);
-    ac.setRaw(results->value);  // Midea uses value instead of state.
-    description = ac.toString();
-  }
-#endif  // DECODE_MIDEA
-#if DECODE_HAIER_AC
-  if (results->decode_type == HAIER_AC) {
-    IRHaierAC ac(0);
-    ac.setRaw(results->state);
-    description = ac.toString();
-  }
-#endif  // DECODE_HAIER_AC
-#if DECODE_HAIER_AC_YRW02
-  if (results->decode_type == HAIER_AC_YRW02) {
-    IRHaierACYRW02 ac(0);
-    ac.setRaw(results->state);
-    description = ac.toString();
-  }
-#endif  // DECODE_HAIER_AC_YRW02
-#if DECODE_SAMSUNG_AC
-  if (results->decode_type == SAMSUNG_AC) {
-    IRSamsungAc ac(0);
-    ac.setRaw(results->state);
-    description = ac.toString();
-  }
-#endif  // DECODE_SAMSUNG_AC
-#if DECODE_COOLIX
-  if (results->decode_type == COOLIX) {
-    IRCoolixAC ac(0);
-    ac.setRaw(results->value);  // Coolix uses value instead of state.
-    description = ac.toString();
-  }
-#endif  // DECODE_COOLIX
-#if DECODE_PANASONIC_AC
-  if (results->decode_type == PANASONIC_AC &&
-      results->bits > kPanasonicAcShortBits) {
-    IRPanasonicAc ac(0);
-    ac.setRaw(results->state);
-    description = ac.toString();
-  }
-#endif  // DECODE_PANASONIC_AC
 #if DECODE_HITACHI_AC
   if (results->decode_type == HITACHI_AC) {
     IRHitachiAc ac(0);
@@ -208,13 +112,6 @@ void dumpACInfo(decode_results *results) {
     description = ac.toString();
   }
 #endif  // DECODE_HITACHI_AC
-#if DECODE_WHIRLPOOL_AC
-  if (results->decode_type == WHIRLPOOL_AC) {
-    IRWhirlpoolAc ac(0);
-    ac.setRaw(results->state);
-    description = ac.toString();
-  }
-#endif  // DECODE_WHIRLPOOL_AC
   // If we got a human-readable description of the message, display it.
   if (description != "") Serial.println("Mesg Desc.: " + description);
 }
@@ -242,7 +139,7 @@ void loop() {
   if (irrecv.decode(&results)) {
     // Display a crude timestamp.
     uint32_t now = millis();
-    Serial.printf("Timestamp : %06u.%03u\n", now / 1000, now % 1000);
+//    Serial.printf("Timestamp : %06u.%03u\n", now / 1000, now % 1000);
     if (results.overflow)
       Serial.printf(
           "WARNING: IR code is too big for buffer (>= %d). "
@@ -255,17 +152,17 @@ void loop() {
     yield();  // Feed the WDT as the text output can take a while to print.
 
     // Display the library version the message was captured with.
-    Serial.print("Library   : v");
-    Serial.println(_IRREMOTEESP8266_VERSION_);
-    Serial.println();
+//    Serial.print("Library   : v");
+//    Serial.println(_IRREMOTEESP8266_VERSION_);
+//    Serial.println();
 
     // Output RAW timing info of the result.
-    Serial.println(resultToTimingInfo(&results));
-    yield();  // Feed the WDT (again)
+//    Serial.println(resultToTimingInfo(&results));
+//    yield();  // Feed the WDT (again)
 
     // Output the results as source code
-    Serial.println(resultToSourceCode(&results));
-    Serial.println("");  // Blank line between entries
-    yield();             // Feed the WDT (again)
+//    Serial.println(resultToSourceCode(&results));
+//    Serial.println("");  // Blank line between entries
+//    yield();             // Feed the WDT (again)
   }
 }
